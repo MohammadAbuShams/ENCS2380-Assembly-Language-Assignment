d897a9e339b5ce8f8abf863a995c2bcbff5c9884
Mohammad AbuShams <116977597+MohammadAbuShams@users.noreply.github.com>
2024-03-23 14:10:19 +0200

    Add files via upload

---
+++ proj.s
@@ -0,0 +1,100 @@
+   ; Mohammad Naeem Abu Shams.
+   ; 1200549.
+   ; ORGA Assignment.
+
+
+    AREA RESET, DATA, READONLY
+	EXPORT __Vectors
+	
+__Vectors	DCD 0x20001000
+	
+			DCD Reset_Handler
+				
+		ALIGN
+	
+	AREA mycode, CODE, READONLY
+	ENTRY
+	EXPORT Reset_Handler
+Reset_Handler
+
+       MOV R0, #10 ; Store 10 decimal to the register R0.
+	   
+
+SUM SPACE 3 ; Define an variable called SUM with size equal 3 byte.
+SUM2 SPACE 3 ; Define a second variable called SUM2 with size equal 3 byte.
+Result SPACE 10 ; Define a third variable called Result with size equal 10 byte.
+  
+	
+      LDR R1, =Array ; To bring the address of the array and stored it into a register R1.
+	  MOV R3, #0 ; Store 0 decimal to the register R3.
+	 
+Array DCB 34, 56, 27, 156, 200, 68, 128, 235, 17, 45 ; Define an array with size equal 10 byte.
+
+
+Loop ; Loop to calculate the sum of numbers in the array;
+	   LDRB R2, [R1] ; To bring one byte from array and stored it into a register R2.
+	   ADD R3, R3, R2 ; Add the numbers in register R2 and R3 then the sum is in register R3.
+	   ADD R1, #1 ; Store 1 decimal to the register R1.
+	   
+	   SUBS R0, R0, #1 ; Decrease the value of register R1 by one(1) and set the flags.
+	   BNE Loop ; Keep looping if Z = 0;
+	   
+	   LDR R1, =SUM ; To bring the address of the SUM and stored it into a register R1.
+	   STR R3, [R1] ; Stroe the value of R3 to SUM.
+	   
+	   LDR R1, =Array ; To bring the address of the array and stored it into a register R1.
+         MOV R0, #10 ; Store 10 decimal to the register R0.
+	   MOV R4, #0 ;Store 0 decimal to the register R4.
+	   
+loop2 ; Loop to find the sum of even numbers in the array. 
+	   
+	   LDRB R2, [R1] ; To bring one byte from array and stored it into a register R2.
+
+	   MOVS R3, R2, LSL #31 ; Shift left register R2 by 31 bit, then set the flags. If Z=0, then the number is odd, else if the Z=1, then the number is even.
+	   BNE Skip; If Z=0, branch ( Skip the add ).
+	   ADD R4, R4, R2; Add the number of register R2 to the register R4, and the result in the register R4.
+Skip
+	   ADD R1, #1 ; Increment the register R1 by 1.
+	   SUBS R0, R0, #1 ; Decrease the value of register R1 by one(1) and set the flags.
+
+	   BNE loop2 ; Keep looping if Z = 0.
+	   
+	   LDR R1, =SUM2 ; To bring the address of the SUM2 and stored it into a register R1.
+	   STR R4, [R1] ; Stroe the value of R4 to SUM2.
+	   
+	   LDR R1, =Array; To bring the address of the array and stored it into a register R1.
+	   LDR R4, =Result ; To bring the address of the Result and stored it into a register R4.
+	   MOV R0, #10 ; Store 10 decimal to the register R0.
+	   
+Loop3 ; Loop to find the largest power of two for each numbers of array.
+
+	   LDRB R2, [R1]  ;To bring one byte from array and stored it into a register R2.
+	   BL Pow ; Branch and link. 
+	   STRB R7, [R4] ; Store one byte to the result arrray.
+	   ADD R4, R4, #1 ; Add one to the register R4, and the result in the register R4.
+	   
+	   ADD R1, #1 ; Increment the register R1 by 1.
+	   SUBS R0, R0, #1 ; Decrease the value of register R1 by one(1) and set the flags.
+	   BNE Loop3  ; Keep looping if Z = 0.
+	   
+Pow ; Function to find the largest power of two.   
+	   MOV R5, #-1 ; Store -1 decimal to the register R5.
+Loop4 ; To find the number of zeros in the right.
+	   ADD R5, R5, #1 ; Add one to the register R5, and the result in the register R5.
+	   ANDS R6, R2,#0x1 ; And between the register R2 and 1 and save the result in R6, then set the flag.
+	   MOV R2, R2, LSR #1 ; Shift left register R2 by 1 bit.
+	   BEQ Loop4 ; Branch if Z=1; 
+	   
+	   MOV R7, #1 ; Store 1 decimal to the register R7.
+	   MOV R7, R7, LSL R5 ; Logical shift register by R5.
+	   
+	   BX LR ; Return.
+	   
+
+	   
+	END ; The end of programme.
+		
+	
+	
+	
+	
